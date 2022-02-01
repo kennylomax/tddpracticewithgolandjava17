27b1d2b6941562f05f63d0e866e374a146652cad
kennylomax <kennylomax@gmail.com>
2022-02-01 10:51:04 +0100

    Application running and tested

--- src/main/java/com/tdd/practice/gol/GolApplication.java
+++ src/main/java/com/tdd/practice/gol/GolApplication.java
@@ -1,13 +1,16 @@
 package com.tdd.practice.gol;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GolApplication.class, args);
+		int generations = 10;
+		GOL gol = new GOL(10,"0000000000000000000000000000000000000000000010000000011100000000000000000000000000000000000000000000");
+		while ( generations-- >0 ){
+			System.out.println( gol.getBoard().replaceAll("/n","\n").replaceAll("1","X")+"\n");
+			gol = gol.evolve();
+		}
 	}
-
 }
--- src/test/java/com/tdd/practice/gol/GolApplicationTests.java
+++ src/test/java/com/tdd/practice/gol/GolApplicationTests.java
@@ -1,13 +1,42 @@
 package com.tdd.practice.gol;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 @SpringBootTest
 class GolApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+
+	@BeforeEach
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
 	}
+
+	@AfterEach
+	public void restoreStreams() {
+		System.setOut(originalOut);
+		System.setErr(originalErr);
+	}
+
+	@Test
+	void testGOLGame() throws InterruptedException {
+        GolApplication.main( new String[]{}) ;
+		assertTrue( outContent.toString().contains(
+			"0-0-0-0-0-0-0-0-0-0\n0-0-0-0-0-0-0-0-0-0\n0-0-0-X-X-X-0-0-0-0\n0-0-0-0-0-0-0-0-0-0\n0-X-0-0-0-0-0-X-0-0\n0-X-0-0-0-0-0-X-0-0\n0-X-0-0-0-0-0-X-0-0\n0-0-0-0-0-0-0-0-0-0\n0-0-0-X-X-X-0-0-0-0\n0-0-0-0-0-0-0-0-0-0\n" ));
+    }
 
 }
